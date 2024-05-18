d709602e7caf02b3c6e1d78d37a736522ecd3e57
Alexey Vlasov <a-vlasov@webjox.ru>
2024-05-18 11:09:01 +0300

    favicon

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,8 @@ const Home = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <Head>
-        <title>Наше Приложение</title>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <title>Berserk assistant</title>
       </Head>
       <main className="mx-2 p-6 max-w-lg md:max-w-2xl mx-auto bg-white rounded-xl shadow-md overflow-hidden transform transition duration-500 hover:scale-105">
         <div className="md:flex">
@@ -34,17 +35,17 @@ const Home = () => {
             <div className="text-center md:text-left">
               <a
                 href={'https://disk.yandex.ru/d/_ZpqZ02copkamw'}
-                download
-                className={`py-2 px-4 rounded-full text-white transition-colors duration-300 ${hover ? 'bg-blue-700' : 'bg-blue-500'
-                  }`}
                 onMouseEnter={() => setHover(true)}
                 onMouseLeave={() => setHover(false)}
+                target='_blank'
               >
-                Скачать .apk
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
-                </svg>
-
+                <button className={`py-2 px-4 rounded-full text-white transition-colors duration-300 flex flex-row gap-2 ${hover ? 'bg-blue-700' : 'bg-blue-500'
+                  }`}>
+                  Скачать .apk c Яндекс.Диск
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                  </svg>
+                </button>
               </a>
             </div>
           </div>
