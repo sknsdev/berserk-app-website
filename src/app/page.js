b8f6dc2077c4b876ff2193f7ddf3a452a74c9ae1
Alexey Vlasov <a-vlasov@webjox.ru>
2024-05-18 10:59:48 +0300

    change logic of apk download

--- src/app/page.js
+++ src/app/page.js
@@ -33,7 +33,7 @@ const Home = () => {
             </p>
             <div className="text-center md:text-left">
               <a
-                href={'/beta-release-Berserk-Assistant.apk'}
+                href={'https://disk.yandex.ru/d/_ZpqZ02copkamw'}
                 download
                 className={`py-2 px-4 rounded-full text-white transition-colors duration-300 ${hover ? 'bg-blue-700' : 'bg-blue-500'
                   }`}
@@ -41,6 +41,10 @@ const Home = () => {
                 onMouseLeave={() => setHover(false)}
               >
                 Скачать .apk
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                </svg>
+
               </a>
             </div>
           </div>
